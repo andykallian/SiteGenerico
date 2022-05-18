e40ed6ac28e1de33ec5a7931777817218ee486b0
Anderson Vianna <andersonkallian@yahoo.com.br>
2022-05-18 16:20:16 -0300

    versao1.0

---
+++ main.js
@@ -0,0 +1,10 @@
+$('.slider-principal').slick({
+    dots: true,
+    Infinity: true,
+    speed: 700,
+    slideToShow: 4,
+    adaptativeHeight:true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+});
+
